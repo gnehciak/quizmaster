345f25027dd3499e3bfd031d4a3afe31bdd7318b
Kaicheng Li <lkc364636722@gmail.com>
2025-12-25 06:17:09 +0000

    File changes

--- src/pages/ReviewAnswers.jsx
+++ src/pages/ReviewAnswers.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { base44 } from '@/api/base44Client';
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { Link } from 'react-router-dom';
 import { createPageUrl } from '@/utils';
 import { Button } from '@/components/ui/button';
